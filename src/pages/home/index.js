5be3c10102de8b47bb8b3cb98f2708290a307de4
Eronar Alves <alveseronar@gmail.com>
2022-07-21 16:11:16 -0300

    create dark mode

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Styles
 import {
   Container,
   Content,
-  ContainerGoals
+  ContainerGoals,
+  DarkMode,
+  Span
 } from "./styles"
 
 // Components
@@ -12,8 +14,16 @@ import CardGoals from "../../Components/CardGoals";
 // Ultis
 import {Goals} from "../../Ultis"
 
+// Icons
+import {FaMoon} from "react-icons/fa"
+import {GiBedLamp} from "react-icons/gi"
+
 export default function Home() {
   const [goalsProfile, setGoalsProfile] = useState(Goals)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const darkContainer = useRef()
+  const darkContent = useRef()
 
   useEffect(() => {
     const storagePosters = JSON.parse(localStorage.getItem("goal")) || []
@@ -27,13 +37,36 @@ export default function Home() {
     
   }, [])
 
+  // DarkMode
+  useEffect(() => {
+
+    if(darkMode) {
+      darkContainer.current.style.backgroundColor = "#121214"
+      darkContainer.current.style.color = "#E1E1E6"
+
+      darkContent.current.style.backgroundColor = "#121214"
+      darkContent.current.style.color = "#E1E1E6"
+    } else {
+      darkContainer.current.style.backgroundColor = "#fff"
+      darkContainer.current.style.color = "#121214"
+
+      darkContent.current.style.backgroundColor = "#fff"
+      darkContent.current.style.color = "#121214"
+    }
+    
+  }, [darkMode])
+
+  const mudarTema = () => {
+    setDarkMode(!darkMode)
+  }
+
   const openGoalsPage = (id) => {
     localStorage.setItem("goal", JSON.stringify(id))
   }
 
   return (
-    <Container>
-      <Content>
+    <Container ref={darkContainer}>
+      <Content ref={darkContent}>
         <h1>Os Objetivos de Desenvolvimento Sustentável em Inhaúma</h1>
         <p>Os Objetivos de Desenvolvimento Sustentável são um apelo global à ação para acabar com a pobreza, proteger o meio ambiente e o clima e garantir que as pessoas, em todos os lugares, possam desfrutar de paz e de prosperidade. Estes são os objetivos para os quais as Nações Unidas estão contribuindo a fim de que possamos atingir a Agenda 2030 no Brasil.</p>
 
@@ -43,6 +76,13 @@ export default function Home() {
           ))}
         </ContainerGoals>
       </Content>
+      <DarkMode onClick={() => {mudarTema()}} darkMode={darkMode}>
+        {darkMode ? <GiBedLamp size={20}/> : <FaMoon size={20}/>}
+          <Span>
+            <span></span>
+            Modo {darkMode ? "Claro" : "Escuro"} 
+          </Span>
+      </DarkMode>
     </Container>
   )
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -2,14 +2,15 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+
+  position: relative;
 
 `
 
 export const Content = styled.div`
   width: 100%;
   max-width: 1200px;
-  height: 100vh;
   margin-inline: auto;
   padding: 20px;
 
@@ -36,4 +37,51 @@ export const ContainerGoals = styled.div`
   @media(max-width: 940px) {
     justify-content: space-evenly;
   }
+`
+
+export const DarkMode = styled.button`
+  height: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  display: flex;
+  align-items: center;
+
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  cursor: pointer;
+
+  padding-inline: 20px;
+  border-radius: 25px;
+  border: none;
+  z-index: 2;
+
+  background-color: ${({ darkMode }) =>
+    darkMode ? "#E1E1E6" : "#121214"};
+  color: ${({ darkMode }) =>
+    darkMode ? "#121214" : "#E1E1E6"};
+
+  &:hover {
+
+    & > span {
+      max-width: 100px;
+      transition: all .6s linear;
+    }
+  }
+
+`
+
+
+export const Span = styled.span`
+  max-width: 0;
+  overflow: hidden;
+
+  span {
+    margin-left: 12px;
+  }
+  
 `
